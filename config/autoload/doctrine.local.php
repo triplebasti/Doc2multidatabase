9e3ae599826af7dd466f8b315326c8e66dab1a31
s.krebs <s.krebs@unister2170d>
2013-10-04 14:46:49 +0200

    asd

---
+++ config/autoload/doctrine.local.php
@@ -0,0 +1,29 @@
+<?php
+return array(
+    'doctrine' => array(
+        'connection' => array(
+            //default connection (configured by default for DoctrineORMModule)
+            'orm_default' => array(
+                'driverClass' => 'Doctrine\DBAL\Driver\PDOMySql\Driver',
+                'params' => array(
+                    'host' => '127.0.0.1',
+                    'port' => '3306',
+                    'user' => 'root',
+                    'password' => '',
+                    'dbname' => 'site',
+                )
+            ),
+            //connection used for shop module
+            'orm_shop' => array(
+                'driverClass' => 'Doctrine\DBAL\Driver\PDOMySql\Driver',
+                'params' => array(
+                    'host' => '127.0.0.1',
+                    'port' => '3306',
+                    'user' => 'root',
+                    'password' => '',
+                    'dbname' => 'shop',
+                )
+            )
+        )
+    )
+);
